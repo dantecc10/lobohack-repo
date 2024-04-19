09d187d6a807168549853a6d449baf5e2c8bb05c
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-04-19 12:58:35 -0600

    working on json outpu

--- php scripts/api-testing.php
+++ php scripts/api-testing.php
@@ -5,20 +5,14 @@ $dbname = 'lobohack';
 $username = 'lobohacker';
 $password = 'SQLTester23!!';
 
-try {
-    // Conexión a la base de datos utilizando PDO
-    $db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    // Consulta SQL para obtener datos de la base de datos
-    $query = "SELECT * FROM users";
-    $statement = $db->query($query);
-    $users = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-    // Devolver los datos en formato JSON
-    header('Content-Type: application/json');
-    echo json_encode($users);
-} catch (PDOException $e) {
-    // Manejo de errores
-    echo "Error: " . $e->getMessage();
+// Devolver los datos en formato JSON
+header('Content-Type: application/json');
+echo json_encode($users);
+$query = "SELECT * FROM users";
+$result = $db->query($query);
+$users = array();
+while ($row = $result->fetch_assoc()) {
+    $users[] = $row;
 }
+
+echo json_encode($users);
