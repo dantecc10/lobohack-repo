60cc07f3931a399262de1d4aa9a65e562dca7f81
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-04-19 17:09:19 -0600

    a ver

--- assets/js/fetcher.js
+++ assets/js/fetcher.js
@@ -33,7 +33,7 @@ function fetchData() {
 // Llamar a la función para hacer la petición AJAX
 //fetchData();
 function log_cars() {
-    data = (fetchData());
+    data[] = (fetchData());
     console.log("Hay " + data[0].cars_quantity_semaphore + " autos en la calle.");
 }
 // Call the updateGoals function every 5 seconds
