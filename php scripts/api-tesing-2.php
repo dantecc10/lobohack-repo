d762f5313558af543ee30646f4aeb069ffd6a5d0
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-04-19 17:12:57 -0600

    ..

--- php scripts/api-tesing-2.php
+++ php scripts/api-tesing-2.php
@@ -8,12 +8,12 @@ $password = 'SQLTester23!!';
 // Devolver los datos en formato JSON
 header('Content-Type: application/json');
 http_response_code(200);
-$query = "SELECT * FROM semaphores";
+$query = "SELECT * FROM users";
 $db = new mysqli($host, $username, $password, $dbname);
 $result = $db->query($query);
 $users = array();
 while ($row = $result->fetch_assoc()) {
-    $semaphores[] = $row;
+    $users[] = $row;
 }
 
-echo json_encode($semaphores);
+echo json_encode($users);
